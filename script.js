e83e56c8189e4d26b67dd01a2253cbbcf7e9ef2d
megz15 <meghraj.g16@gmail.com>
2022-05-11 00:00:41 +0530

    Coded procedurally generated grid

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+    let gridLen = prompt('Enter grid length:', 16);
+    // let gridLen = 16;
+    
+    let grid = document.getElementById('draw-grid-container');
+    grid.style.gridTemplateColumns = 'auto '.repeat(gridLen)
+    
+    for (let i = 0; i < gridLen; i++) {
+        let b = document.createElement('div');
+        b.classList.add('draw-grid-col');
+        grid.appendChild(b);
+
+        for (let j = 0; j < gridLen; j++) {
+            let a = document.createElement('div');
+            a.classList.add('draw-grid-item');
+            a.textContent = gridLen*j + i;
+            b.appendChild(a);
+        }
+    }
+}
