2856d29924290fbbd6ef40043041b3781b6694ae
megz15 <meghraj.g16@gmail.com>
2022-05-11 00:16:43 +0530

    Added basic drawing feature

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-window.onload = () => {
-    let gridLen = prompt('Enter grid length:', 16);
-    // let gridLen = 16;
-    
-    let grid = document.getElementById('draw-grid-container');
-    grid.style.gridTemplateColumns = 'auto '.repeat(gridLen)
-    
-    for (let i = 0; i < gridLen; i++) {
-        let b = document.createElement('div');
-        b.classList.add('draw-grid-col');
-        grid.appendChild(b);
+let gridLen = prompt('Enter grid length:', 16);
+// let gridLen = 16;
 
-        for (let j = 0; j < gridLen; j++) {
-            let a = document.createElement('div');
-            a.classList.add('draw-grid-item');
-            a.textContent = gridLen*j + i;
-            b.appendChild(a);
-        }
+let grid = document.getElementById('draw-grid-container');
+grid.style.gridTemplateColumns = 'auto '.repeat(gridLen)
+
+for (let i = 0; i < gridLen; i++) {
+    let b = document.createElement('div');
+    b.classList.add('draw-grid-col');
+    grid.appendChild(b);
+
+    for (let j = 0; j < gridLen; j++) {
+        let a = document.createElement('div');
+        a.classList.add('draw-grid-item');
+        a.textContent = gridLen*j + i;
+        b.appendChild(a);
     }
 }
+
+
+Array.from(document.getElementsByClassName('draw-grid-item')).forEach(e => {
+    e.addEventListener('mouseover', function(){
+        this.style.backgroundColor = 'black';
+    })
+});
