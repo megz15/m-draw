af4b2812c211457d5aba6fa3c119364cc2cc5c53
megz15 <meghraj.g16@gmail.com>
2022-05-11 16:35:35 +0530

    Added random colour functionality

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 let drawColour = 'black';
 let isClick = false;
+let isRandomColour = false;
 
-setDrawColour = (col) => drawColour = col;
+let randomColourButton = document.getElementById('random-colour-button');
+
+function setDrawColour(col) {
+    selectRandomColour()
+    drawColour = col;
+}
 
 function selectErasor() {
+    selectRandomColour()
     drawColour = 'white';
     document.getElementById('colour-picker-input').value='#ffffff';
+}
+
+// const randomHsl = () => `hsla(${Math.random() * 360}, 100%, 50%, 1)`
+function setRandomDrawColour() {
+    if (isRandomColour) {
+        drawColour = '#' + (Math.random().toString(16) + "000000").substring(2,8)
+        document.getElementById('colour-picker-input').value = drawColour;
+    }
+}
+
+function selectRandomColour() {
+    if (randomColourButton.textContent === 'Stop') {
+        isRandomColour = false;
+        randomColourButton.textContent = 'Random Colours';
+    } else {
+        isRandomColour = true;
+        randomColourButton.textContent = 'Stop';
+    }
 }
 
 addEventListener('mousedown', () => isClick = true);
@@ -28,13 +53,17 @@ function drawGrid(gridLen) {
 
             a.addEventListener('mousedown', function() {
                 isClick = true;
+                setRandomDrawColour()
                 this.style.backgroundColor = drawColour;
             });
             
             a.addEventListener('mouseup', () => isClick = false);
 
             a.addEventListener('mouseenter', function(){
-                if (isClick) this.style.backgroundColor = drawColour;
+                if (isClick) {
+                    setRandomDrawColour()
+                    this.style.backgroundColor = drawColour;
+                }
             })
 
             a.classList.add('draw-grid-item');
