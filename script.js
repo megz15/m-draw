ba2d22ded9069387fad91a5ecbaab021af6ceb94
megz15 <meghraj.g16@gmail.com>
2022-05-11 16:09:54 +0530

    Added option to change grid size

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 let drawColour = 'black';
+// let gridItemSide = 20;
 let isClick = false;
 // let gridLen = prompt('Enter grid length:', 16);
-let gridLen = 16;
+// let gridLen = 16;
 
 setDrawColour = (col) => drawColour = col;
 
 addEventListener('mousedown', () => isClick = true);
 addEventListener('mouseup', () => isClick = false);
 
-let grid = document.getElementById('draw-grid-container');
-grid.style.gridTemplateColumns = 'auto '.repeat(gridLen)
+function drawGrid(gridLen) {
+    let grid = document.getElementById('draw-grid-container');
 
-for (let i = 0; i < gridLen; i++) {
-    let b = document.createElement('div');
-    b.classList.add('draw-grid-col');
-    grid.appendChild(b);
+    grid.innerHTML = '' //clear previous grid
 
-    for (let j = 0; j < gridLen; j++) {
-        let a = document.createElement('div');
+    grid.style.gridTemplateColumns = 'auto '.repeat(gridLen)
 
-        a.addEventListener('mousedown', function() {
-            isClick = true;
-            this.style.backgroundColor = drawColour;
-        });
-        
-        a.addEventListener('mouseup', () => isClick = false);
+    for (let i = 0; i < gridLen; i++) {
+        let b = document.createElement('div');
+        b.classList.add('draw-grid-col');
+        grid.appendChild(b);
 
-        a.addEventListener('mouseenter', function(){
-            if (isClick) this.style.backgroundColor = drawColour;
-        })
+        for (let j = 0; j < gridLen; j++) {
+            let a = document.createElement('div');
 
-        a.classList.add('draw-grid-item');
-        a.textContent = gridLen*j + i;
-        b.appendChild(a);
+            a.addEventListener('mousedown', function() {
+                isClick = true;
+                this.style.backgroundColor = drawColour;
+            });
+            
+            a.addEventListener('mouseup', () => isClick = false);
+
+            a.addEventListener('mouseenter', function(){
+                if (isClick) this.style.backgroundColor = drawColour;
+            })
+
+            a.classList.add('draw-grid-item');
+            // a.style.width = a.style.height = gridItemSide + 'px';
+            a.textContent = gridLen*j + i;
+            b.appendChild(a);
+        }
     }
 }
+
+drawGrid(16);
